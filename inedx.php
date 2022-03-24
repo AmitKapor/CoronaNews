4cc4a9517b222a80a206eb41387d5f7ba8467883
Amit Kapoor <amitkapoor2211973@gmail.com>
2022-03-24 23:41:21 +0530

    MVP ready app

---
+++ inedx.php
@@ -0,0 +1,408 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Document</title>
+    <?php include 'link/links.php';?>
+    <?php include 'css/style.php';?>
+</head>
+<body>
+
+  <nav class="navbar navbar-expand-lg nav_style p-3">
+    <a class="navbar-brand pl-5" href="#">COVID-19</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto pr-5 text-capitalize">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#aboutid">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#sympid">symptoms</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#preventid">Prevention Measures</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#contactid">Contact Us</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <div class="main_header ">
+    <div class="row w-100 h-100">
+      <div class="col-lg-5 col-md-5 col-12 order-lg-1 order-2">
+          <div class="leftside w-100 h-100 d-flex justify-content-center align-items-center">
+            <img src="images/star.webp" width="300" height="300">
+          </div>
+      </div>
+
+      <div class="col-lg-7 col-md-7 col-12 order-lg-2 order-1">
+          <div class ="rightside w-100 h-100 d-flex justify-content-center align-items-center">
+            <h1>Let's Stay Safe And Fight Together Against this cor<span class="corona_rota"><img src="images/coronav.png" width="150" height="150"></span>na vrius</h1>
+
+          </div>
+      </div>
+
+    </div>
+  </div>
+
+  <!--******** corona latest updates ***********-->
+
+  <section class="corona_update">
+    <div class="mb-3">
+      <h3 class="text-uppercase text-center">covid-19 cases</h3>
+    </div>
+
+    <div class="d-flex justify-content-around align-items-center">
+      <div>
+        <h1 class="count">4,27,00000</h1>
+        <p>Total Cases in INDIA</p>
+      </div>
+
+      <div>
+        <h1 class="count">4,23,127</h1>
+        <p>Active Corona Cases*</p>
+      </div>
+
+      <div>
+        <h1 class="count">41,76,0458</h1>
+        <p>Discharged</p>
+      </div>
+
+      <div>
+        <h1 class="count">5,09,358</h1>
+        <p>Deaths</p>
+      </div>
+
+      <div>
+        <h1 class="count">1,73,42,62,440</h1>
+        <p>Total Vaccinations</p>
+      </div>
+
+    </div>
+  </section>
+
+<!--******** about section ***********-->
+
+<div class="container-fluid sub_section pt-5 pb-5" id="aboutid">
+
+  <div class ="section_header text-center mb-5 mt-4">
+    <h1>About COVID-19</h1>
+  </div>
+
+  <div class="row pt-5">
+    <div class="col-lg-5 col-md-6 col-12 ml-5">
+      <img src="images/about.jpg" class="img-fluid">
+    </div>
+    <div class="col-lg-6 col-md-6 col-12">
+      <h2>What is Corona Virus</h2>
+      <p>Coronavirus disease (COVID-19) is an infectious disease caused by the SARS-CoV-2 virus.Most people infected with the virus will experience mild to moderate respiratory illness and recover without requiring special treatment. However, some will become seriously ill and require medical attention. Older people and those with underlying medical conditions like cardiovascular disease, diabetes, chronic respiratory disease, or cancer are more likely to develop serious illness. Anyone can get sick with COVID-19 and become seriously ill or die at any age</p>
+      <p>The virus can spread from an infected person’s mouth or nose in small liquid particles when they cough, sneeze, speak, sing or breathe. These particles range from larger respiratory droplets to smaller aerosols.</p>
+    </div>
+  </div>
+</div>
+
+<!-- symptoms -->
+
+<div class="container-fluid pt-5 pb-5" id="sympid">
+
+  <div class ="section_header text-center mb-5 mt-4">
+    <h1>CoronaVirus Symptoms</h1>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-12 mt-5">
+        <figure class="text-center">
+        <img src="images/cough.jpg" class="img-fluid" height="120" width="120">
+        <figcaption>Cough</figcaption>
+        </figure>
+      </div>
+
+      <div class="col-lg-4 col-md-4 col-12 mt-5">
+        <figure class="text-center">
+        <img src="images/sore.jpg" class="img-fluid" height="120" width="120">
+        <figcaption>Sore throat</figcaption>
+        </figure>
+      </div>
+
+      <div class="col-lg-4 col-md-4 col-12 mt-5">
+        <figure class="text-center">
+        <img src="images/runny.jpg" class="img-fluid" height="120" width="120">
+        <figcaption>Runny nose</figcaption>
+        </figure>
+      </div>
+
+      <div class="col-lg-4 col-md-4 col-12 mt-5">
+        <figure class="text-center">
+        <img src="images/fever.jpg" class="img-fluid" height="120" width="120">
+        <figcaption>Fever</figcaption>
+        </figure>
+      </div>
+
+      <div class="col-lg-4 col-md-4 col-12 mt-5">
+        <figure class="text-center">
+        <img src="images/tierd1.jpg" class="img-fluid" height="120" width="120">
+        <figcaption>Tierdness</figcaption>
+        </figure>
+      </div>
+
+      <div class="col-lg-4 col-md-4 col-12 mt-5">
+        <figure class="text-center">
+        <img src="images/breathe2.jpg" class="img-fluid" height="120" width="120">
+        <figcaption>Difficulty in breathing(sever cases)</figcaption>
+        </figure>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!--prevention section-->
+<div class="container-fluid sub_section pt-5 pb-5" id="preventid">
+
+  <div class ="section_header text-center mb-5 mt-4">
+    <h1>Step For Prevention Agaisnt Coronavirus</h1>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-12 mt-5">
+        <div class="row">
+          <div class="col-lg-4 col-md-4 col-12 mt-5">
+          <figure class="text-center">
+            <img src="images/wash.png" class="img-fluid" height="90" width="90">
+          </figure>
+          </div>
+          <div class="col-lg-8 col-md-8 col-12 mt-5">
+            <p>Wash your hands regularly for 20 sec with soap and water or any alcohol-based hand rub</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-4 col-12 mt-5">
+        <div class="row">
+          <div class="col-lg-4 col-md-4 col-12 mt-5">
+          <figure class="text-center">
+            <img src="images/distance.png" class="img-fluid" height="90" width="90">
+          </figure>
+          </div>
+          <div class="col-lg-8 col-md-8 col-12 mt-5">
+            <p>If you are not up to date on COVID-19 vaccines, stay at least 6 feet away from other people, especially if you are at higher risk of getting very sick with COVID-19</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-4 col-12 mt-5">
+        <div class="row">
+          <div class="col-lg-4 col-md-4 col-12 mt-5">
+          <figure class="text-center">
+            <img src="images/mask.png" class="img-fluid" height="90" width="90">
+          </figure>
+          </div>
+          <div class="col-lg-8 col-md-8 col-12 mt-5">
+            <p>Wear a mask with the best fit, protection, and comfort for you. If you are in an area with a high COVID-19 Community Level and are ages 2 or older, wear a mask indoors in public.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-4 col-12 mt-5">
+        <div class="row">
+          <div class="col-lg-4 col-md-4 col-12 mt-5">
+          <figure class="text-center">
+            <img src="images/vaccine.jpg" class="img-fluid" height="90" width="90">
+          </figure>
+          </div>
+          <div class="col-lg-8 col-md-8 col-12 mt-5">
+            <p>Getting vaccinated is the best way to slow the spread of SARS-CoV-2, the virus that causes COVID-19.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-4 col-12 mt-5">
+        <div class="row">
+          <div class="col-lg-4 col-md-4 col-12 mt-5">
+          <figure class="text-center">
+            <img src="images/stay home.jpg" class="img-fluid" height="90" width="90">
+          </figure>
+          </div>
+          <div class="col-lg-8 col-md-8 col-12 mt-5">
+            <p>Stay at home and in case of any emergency wear a face mask before going out and use hand sanatizer</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-4 col-12 mt-5">
+        <div class="row">
+          <div class="col-lg-4 col-md-4 col-12  mt-5">
+          <figure class="text-center">
+            <img src="images/isolate.jpg" class="img-fluid" height="90" width="90">
+          </figure>
+          </div>
+          <div class="col-lg-8 col-md-8 col-12 mt-5">
+            <p>f you develop symptoms or test positive for COVID-19, self-isolate until you recover.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<!--CONTACT US PAGE-->
+<div class="container-fluid pt-5 pb-5" id="contactid">
+  <div class ="section_header text-center mb-5 mt-4">
+    <h1>Contact Us</h1>
+  </div>
+</div>
+<div class = "container">
+  <div class="row">
+
+    <div class="col-lg-8 offset-lg-2 col-12">
+    <form action="" method="POST">
+
+    <div class="form-group">
+    <label>Name</label>
+    <input type="text" class="form-control" name="username" placeholder="Name" autocomplete="off" required>
+    </div>
+
+  <div class="form-group">
+    <label>Email</label>
+    <input type="email" class="form-control" name="email" placeholder="name@example.com" required autocomplete="off" >
+  </div>
+
+  <div class="form-group">
+    <label>Mobile</label>
+    <input type="number" class="form-control" name="mobile" placeholder="Mobile" autocomplete="off" required>
+    </div>
+
+    <div class="form-group">
+      <label>Select Symptoms</label><br>
+      <div class="custom-control custom-checkbox custom-control-inline text-capitalize">
+        <input type="checkbox" class="custom-control-input" id="customcheckbox1" name="coronasym[]" value="cold">
+        <label class="custom-control-label" for="customcheckbox1">Cold</label>
+      </div>
+
+      <div class="custom-control custom-checkbox custom-control-inline text-capitalize">
+        <input type="checkbox" class="custom-control-input" id="customcheckbox2" name="coronasym[]" value="fever">
+        <label class="custom-control-label" for="customcheckbox2">Fever</label>
+      </div>
+
+      <div class="custom-control custom-checkbox custom-control-inline text-capitalize">
+        <input type="checkbox" class="custom-control-input" id="customcheckbox3" name="coronasym[]" value="breath">
+        <label class="custom-control-label" for="customcheckbox3">Difficulty in Breathing</label>
+      </div>
+
+      <div class="custom-control custom-checkbox custom-control-inline text-capitalize">
+        <input type="checkbox" class="custom-control-input" id="customcheckbox4" name="coronasym[]" value="tierd">
+        <label class="custom-control-label" for="customcheckbox4">Feeling Weak and Tierd</label>
+      </div>
+
+    </div>
+
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Message</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="msg"></textarea>
+  </div>
+
+  <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+
+</form>
+    </div>
+  </div>
+
+</div>
+
+<!--top cursor-->
+
+<div class ="container scrolltop floar-right pr-5">
+  <i class ="fa fa-arrow-up" onclick="topFunction()" id ="myBtn"></i>
+</div>
+<!--footer-->
+<footer class="mt-5">
+  <div class="footer_style text-black text-center container-fluid">
+    <p>Copyright by Amit Kapoor</p>
+  </div>
+
+</footer>
+
+<script type ="text/javascript">
+
+  $('.count').counterUp(
+    {
+    delay:10,
+    time:3000
+  })
+  mybutton = document.getElementById("myBtn");
+
+  window.onscroll = function()
+  {
+    scrollFunction();
+  }
+
+  function scrollFunction()
+  {
+    if (document.body.scrollTop>100 || document.documentElement.scrollTop > 100)
+    {
+      mybutton.style.display = "block";
+
+    }
+    else
+    {
+      mybutton.style.display = "none";
+    }
+  }
+  function topFunction()
+  {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+</script>
+</body>
+</html>
+
+<?php
+
+include 'dbcon.php';
+
+if(isset($_POST['submit']))
+{
+
+  $username = $_POST['username'];
+  $email = $_POST['email'];
+  $mobile = $_POST['mobile'];
+  $symp = $_POST['coronasym'];
+  $msg = $_POST['msg'];
+
+  $chk = "";
+  foreach($symp as $chk1)
+  {
+    $chk .= $chk1."," ;
+  }
+  $insertquery = " insert into coronacase(username, email, mobile, symptoms, message	) values('$username', '$email','$mobile','$chk','$msg')";
+  $query = mysqli_query($con, $insertquery);
+  
+  if($query)
+{
+    ?>
+    <script>
+        alert("Insertion successful");
+    </script>
+    <?php
+}
+else
+{
+    ?>
+    <script>
+        alert("Insertion not successful");
+    </script>
+    <?php 
+}
+}
+
+
+
+?>
